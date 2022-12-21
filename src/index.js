bdcf77882f11d29b8381a6c42f5d6c214fd4fb70
IceTeaOxO <popopuchang@gmail.com>
2022-12-21 11:32:56 +0800

    增加react router function

--- src/index.js
+++ src/index.js
@@ -2,13 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import Page from './Page'
 import reportWebVitals from './reportWebVitals';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+//設置path，以及對應的元件
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },{
+    path: "/a",
+    element:<App />,
+  },
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
